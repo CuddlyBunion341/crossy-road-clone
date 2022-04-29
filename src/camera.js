41a9282f8a9856c1fed6baf243f1c0f736fc5bd4
CuddlyBunion341 <53896675+CuddlyBunion341@users.noreply.github.com>
2022-04-29 20:01:08 +0200

    Added Tween JS and continous camera movement

--- src/camera.js
+++ src/camera.js
@@ -1,18 +1,55 @@
+import * as TWEEN from "@tweenjs/tween.js";
+
+const animate = time => {
+	requestAnimationFrame(animate);
+	TWEEN.update(time);
+};
+
+animate();
+
 export class CameraController {
 	constructor(camera) {
 		this.camera = camera;
 		this.speed = 0.3;
 		this.active = true;
 		this.playerLane = 0;
+		this.animation = null;
 	}
 
 	jump() {
-		this.playerLane++;
-		this.camera.position.x = this.playerLane;
+		const from = {
+			x: this.camera.position.x,
+			y: this.camera.position.y,
+			z: this.camera.position.z,
+		};
+
+		const to = {
+			x: ++this.playerLane,
+			y: this.camera.position.y,
+			z: this.camera.position.z,
+		};
+
+		this.animation = new TWEEN.Tween(from)
+			.to(to, 100)
+			.easing(TWEEN.Easing.Linear.None)
+			.onUpdate(() => {
+				this.camera.position.x = from.x;
+				this.camera.position.y = from.y;
+				this.camera.position.z = from.z;
+			})
+			.onComplete(() => {
+				this.animation = null;
+			})
+			.start();
 	}
 
 	update(delta) {
 		if (!this.active) return;
+		if (this.animation) {
+			// TWEEN.update(delta);
+			return;
+		}
+
 		this.camera.position.x += this.speed * delta;
 		// test if camera is in front of player
 		if (this.camera.position.x > this.playerLane + 1.5) {
